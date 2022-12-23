9ee5bbefdf7d1a53d92bbce1828171faaa202f80
fitsumlegesse@icloud.com <fitsumlegesse@gmail.com>
2022-12-22 19:39:42 -0800

    added an input fiel and modifies css

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {FaPlusCircle} from 'react-icons/fa'
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <h3 className='title'>ToDo List App</h3>
+        <form className='form-todo'>
+          <input type="text" className='form-control' placeholder='Enter Your ToDo' name="text"/>
+          <button type="button" className='btn'><FaPlusCircle className='icon'/></button>
+        </form>
       </div>
     </div>
   );
