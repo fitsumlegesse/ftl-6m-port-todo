b93fcad3916bf12c232563f16e15160e04e15c9b
fitsumlegesse@icloud.com <fitsumlegesse@gmail.com>
2022-12-26 20:56:52 -0800

    adds three columns and modifies css

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
-import {FaPlusCircle} from 'react-icons/fa'
+import {FaPlusCircle, FaCheck, FaTrash} from 'react-icons/fa'
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-        <h3 className='title'>ToDo List App</h3>
-        <form className='form-todo'>
-          <input type="text" className='form-control' placeholder='Enter Your ToDo' name="text"/>
-          <button type="button" className='btn'><FaPlusCircle className='icon'/></button>
+      <div className="container">
+        <h3 className="title">ToDo List App</h3>
+        <form className="form_todo">
+          <input type="text" className="form-control" placeholder="Enter Your Todo" name="text"/>
+          <button type="button" className="btn"><FaPlusCircle className="icon"/></button>
         </form>
+        <div className="todos_wrapper">
+         <div className="todos_list">
+           <h3 className="todo_title">Todos List</h3>
+         </div>
+         <div className="todos_list">
+           <h3 className="todo_title">InProgress</h3>
+         </div>
+         <div className="todos_list">
+           <h3 className="todo_title">Completed</h3>
+         </div>
+        </div>
       </div>
     </div>
   );
